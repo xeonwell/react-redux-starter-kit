61dfa048cd4b33ff99d06f2aefda416ec7894437
xeonwell <admin@xw.hb.cn>
2017-03-22 19:05:46 +0800

    add pagenotfound route

---
+++ src/routes/PageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+/**
+ * Created by xeonwell on 2017-03-22.
+ */
+
+import React from 'react'
+
+export const PageNotFound = () => (
+  <div>404</div>
+)
+
+export default PageNotFound
---
+++ src/routes/PageNotFound/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by xeonwell on 2017-03-22.
+ */
+
+import PageNotFound from './PageNotFound'
+
+export default {
+  path: '*',
+  component: PageNotFound
+}

f8d6c004b41db76b2d53fc4a9497dfc22f7a5df6
xeonwell <admin@xw.hb.cn>
2017-03-23 18:44:44 +0800

    code format

--- server/mock-server.js
+++ server/mock-server.js
@@ -3,38 +3,39 @@
  */
 
 module.exports = function (options) {
-  let path = require('path')
-  let url = require('url')
-  let fs = require('fs')
-  let mockPath = path.join(__dirname, '../mock')
-  let log = (options && typeof options.log === 'function') ? options.log : console.log
-  let successRate = (options && typeof options.successRate === 'number') ? options.successRate : 1
-  let isSuccessful = () => (Math.random() < successRate)
-  let successWrap = (data) => ({status: 1, data: data})
-  let failWrap = () => ({status: 2, data: 'MockServer random error occurred.'})
-  log('mock enabled. mock path ->', mockPath)
+  let path         = require('path');
+  let url          = require('url');
+  let fs           = require('fs');
+  let mockPath     = path.join(__dirname, '../mock');
+  let log          = (options && typeof options.log === 'function') ? options.log : console.log;
+  let successRate  = (options && typeof options.successRate === 'number') ? options.successRate : 1;
+  let isSuccessful = () => (Math.random() < successRate);
+  let successWrap  = (data) => ({status: 1, data: data});
+  let failWrap     = () => ({status: 2, data: 'MockServer random error occurred.'});
+
+  log('mock enabled. mock path ->', mockPath);
 
   return function (req, res, next) {
     if (!isSuccessful()) {
       // log('random error')
-      res.json(failWrap())
-      return
+      res.json(failWrap());
+      return;
     }
-    let urlObj = url.parse(req.url, true, true)
-    let jsonFile = path.join(mockPath, urlObj.pathname + '.json')
+    let urlObj   = url.parse(req.url, true, true);
+    let jsonFile = path.join(mockPath, urlObj.pathname + '.json');
     fs.readFile(jsonFile, function (err, result) {
       if (err) {
         // log(jsonFile + ' not found. next')
-        return next()
+        return next();
       }
 
-      log(jsonFile + 'find and output')
-      let str = result.toString()
+      log(jsonFile + 'find and output');
+      let str = result.toString();
       try {
-        res.json(successWrap(JSON.parse(str)))
+        res.json(successWrap(JSON.parse(str)));
       } catch (e) {
-        res.send(str)
+        res.send(str);
       }
-    })
-  }
-}
+    });
+  };
+};

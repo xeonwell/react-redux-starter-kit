6b8050a0a5a8921ec2ccef0e4061033043cb7d17
xeonwell <admin@xw.hb.cn>
2017-03-23 18:43:32 +0800

    add http-proxy, add mock-server

--- config/environments.config.js
+++ config/environments.config.js
@@ -1,6 +1,14 @@
 // Here is where you can define configuration overrides based on the execution environment.
 // Supply a key to the default export matching the NODE_ENV that you wish to target, and
 // the base configuration will apply your overrides before exporting itself.
+
+// Define HTTP proxies to your custom API backend(代理到本地服务器)
+// https://github.com/chimurai/http-proxy-middleware
+const proxyOption = {
+  target:       process.env.PROXY_TARGET || 'http://localhost:8910',
+  changeOrigin: true,
+};
+
 module.exports = {
   // ======================================================
   // Overrides when NODE_ENV === 'development'
@@ -8,22 +16,34 @@ module.exports = {
   // NOTE: In development, we use an explicit public path when the assets
   // are served webpack by to fix this issue:
   // http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809
-  development : (config) => ({
-    compiler_public_path : `http://${config.server_host}:${config.server_port}/`
+  development: (config) => ({
+    compiler_public_path: `http://${config.server_host}:${config.server_port}/`,
+    proxyTable:           {
+      '/api':                 Object.assign({pathRewrite: {'^/api': ''}}, proxyOption),
+      '/Content/':            proxyOption,
+      '/UeditorHandler.ashx': proxyOption,
+    },
+    mockConfig:           {
+      enable:  true,
+      options: {
+        successRate: 1,
+        log:         require('debug')('app:MockServer'),
+      },
+    },
   }),
 
   // ======================================================
   // Overrides when NODE_ENV === 'production'
   // ======================================================
-  production : (config) => ({
-    compiler_public_path     : '/',
-    compiler_fail_on_warning : false,
-    compiler_hash_type       : 'chunkhash',
-    compiler_devtool         : null,
-    compiler_stats           : {
-      chunks       : true,
-      chunkModules : true,
-      colors       : true
-    }
-  })
-}
+  production: (config) => ({
+    compiler_public_path:     '/',
+    compiler_fail_on_warning: false,
+    compiler_hash_type:       'chunkhash',
+    compiler_devtool:         null,
+    compiler_stats:           {
+      chunks:       true,
+      chunkModules: true,
+      colors:       true,
+    },
+  }),
+};
--- server/main.js
+++ server/main.js
@@ -1,56 +1,69 @@
-const express = require('express')
-const debug = require('debug')('app:server')
-const path = require('path')
-const webpack = require('webpack')
-const webpackConfig = require('../config/webpack.config')
-const project = require('../config/project.config')
-const compress = require('compression')
+const express       = require('express');
+const debug         = require('debug')('app:server');
+const path          = require('path');
+const webpack       = require('webpack');
+const webpackConfig = require('../config/webpack.config');
+const project       = require('../config/project.config');
+const compress      = require('compression');
 
-const app = express()
+const app = express();
 
 // Apply gzip compression
-app.use(compress())
+app.use(compress());
 
 // ------------------------------------
 // Apply Webpack HMR Middleware
 // ------------------------------------
 if (project.env === 'development') {
-  const compiler = webpack(webpackConfig)
+  const compiler        = webpack(webpackConfig);
+  const proxyMiddleware = require('http-proxy-middleware');
 
-  debug('Enabling webpack dev and HMR middleware')
+  if (project.mockConfig.enable) {
+    app.use(require('./mock-server')(project.mockConfig.options))
+  }
+  // proxy api requests
+  Object.keys(project.proxyTable).forEach(function (context) {
+    let options = project.proxyTable[context];
+    if (typeof options === 'string') {
+      options = {target: options};
+    }
+    app.use(proxyMiddleware(context, options));
+  });
+
+  debug('Enabling webpack dev and HMR middleware');
   app.use(require('webpack-dev-middleware')(compiler, {
-    publicPath  : webpackConfig.output.publicPath,
-    contentBase : project.paths.client(),
-    hot         : true,
-    quiet       : project.compiler_quiet,
-    noInfo      : project.compiler_quiet,
-    lazy        : false,
-    stats       : project.compiler_stats
-  }))
+    publicPath : webpackConfig.output.publicPath,
+    contentBase: project.paths.client(),
+    hot        : true,
+    quiet      : project.compiler_quiet,
+    noInfo     : project.compiler_quiet,
+    lazy       : false,
+    stats      : project.compiler_stats
+  }));
   app.use(require('webpack-hot-middleware')(compiler, {
     path: '/__webpack_hmr'
-  }))
+  }));
 
   // Serve static assets from ~/public since Webpack is unaware of
   // these files. This middleware doesn't need to be enabled outside
   // of development since this directory will be copied into ~/dist
   // when the application is compiled.
-  app.use(express.static(project.paths.public()))
+  app.use(express.static(project.paths.public()));
 
   // This rewrites all routes requests to the root /index.html file
   // (ignoring file requests). If you want to implement universal
   // rendering, you'll want to remove this middleware.
   app.use('*', function (req, res, next) {
-    const filename = path.join(compiler.outputPath, 'index.html')
+    const filename = path.join(compiler.outputPath, 'index.html');
     compiler.outputFileSystem.readFile(filename, (err, result) => {
       if (err) {
-        return next(err)
+        return next(err);
       }
-      res.set('content-type', 'text/html')
-      res.send(result)
-      res.end()
-    })
-  })
+      res.set('content-type', 'text/html');
+      res.send(result);
+      res.end();
+    });
+  });
 } else {
   debug(
     'Server is being run outside of live development mode, meaning it will ' +
@@ -58,12 +71,12 @@ if (project.env === 'development') {
     'do not need an application server for this and can instead use a web ' +
     'server such as nginx to serve your static files. See the "deployment" ' +
     'section in the README for more information on deployment strategies.'
-  )
+  );
 
   // Serving ~/dist by default. Ideally these files should be served by
   // the web server and not the app server, but this helps to demo the
   // server in production.
-  app.use(express.static(project.paths.dist()))
+  app.use(express.static(project.paths.dist()));
 }
 
-module.exports = app
+module.exports = app;
